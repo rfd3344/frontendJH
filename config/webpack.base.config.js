7602a268c734db04608a68c0e2ec638e504f89b5
Jinhui Li <rfd3344@gmail.com>
2019-10-02 23:55:10 +1000

    11

--- config/webpack.base.config.js
+++ config/webpack.base.config.js
@@ -1,100 +1,84 @@
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+	template: './public/index.html',
+	filename: 'index.html',
+	inject: 'body',
+})
 const path = require('path');
-const webpack = require('webpack');
 
-// 配置常量
-// 源代码的根目录（本地物理文件路径）
-const SRC_PATH = path.resolve('./src');
-// 打包后的资源根目录（本地物理文件路径）
-const ASSETS_BUILD_PATH = path.resolve('./build');
-// 资源根目录（可以是 CDN 上的绝对路径，或相对路径）
-const ASSETS_PUBLIC_PATH = '/assets/';
-
-module.exports = {
-  context: SRC_PATH, // 设置源代码的默认根路径
-  resolve: {
-    extensions: ['.js', '.jsx']  // 同时支持 js 和 jsx
-  },
-  entry: {
-    // 注意 entry 中的路径都是相对于 SRC_PATH 的路径
-    vendor: './vendor',
-    a: ['./entry-a'],
-    b: ['./entry-b'],
-    c: ['./entry-c']
-  },
-  output: {
-    path: ASSETS_BUILD_PATH,
-    publicPath: ASSETS_PUBLIC_PATH,
-    filename: './[name].js'
-  },
-  module: {
-    rules: [
-      {
-        enforce: 'pre',  // ESLint 优先级高于其他 JS 相关的 loader
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        loader: 'eslint-loader'
-      },
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        // 建议把 babel 的运行时配置放在 .babelrc 里，从而与 eslint-loader 等共享配置
-        loader: 'babel-loader'
-      },
-      {
-        test: /\.(png|jpg|gif)$/,
-        use:
-        [
-          {
-            loader: 'url-loader',
-            options:
-            {
-              limit: 8192,
-              name: 'images/[name].[ext]'
-            }
-          }
-        ]
-      },
-      {
-        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        use:
-        [
-          {
-            loader: 'url-loader',
-            options:
-            {
-              limit: 8192,
-              mimetype: 'application/font-woff',
-              name: 'fonts/[name].[ext]'
-            }
-          }
-        ]
-      },
-      {
-        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        use:
-        [
-          {
-            loader: 'file-loader',
-            options:
-            {
-              limit: 8192,
-              mimetype: 'application/font-woff',
-              name: 'fonts/[name].[ext]'
-            }
-          }
-        ]
-      }
-    ]
-  },
-  plugins: [
-    // 每次打包前，先清空原来目录中的内容
-  //   new CleanWebpackPlugin([ASSETS_BUILD_PATH], { verbose: false }),
-    // 启用 CommonChunkPlugin
-    // new webpack.optimize.splitChunks({
-    //   names: 'vendor',
-    //   minChunks: Infinity
-    // })
-	//	HtmlWebpackPluginConfig,
-  ]
+const entry = {
+	main: path.resolve(__dirname, '../src/index.js'),
+	entry2: path.resolve(__dirname, '../src/entry2.js'),
 };
+
+const output = {
+	filename: './[name]1bundle.js',
+	publicPath: '/',
+	path: path.resolve(__dirname, '../public/dist'),
+};
+
+// module is Globle variable
+const moduleRules = {
+	rules: [
+		{ 	test: /\.js$/,
+			exclude: /(node_modules|bower_components)/,
+			use: [{ loader: 'babel-loader' }],
+		},
+		{	test: /\.less$/,
+			use: [
+				{ loader: 'style-loader' },
+				{ loader: 'css-loader' },
+				{ loader: 'less-loader' },
+			]
+		},
+		{	test: /\.(png|jpg|gif)$/,
+			use: [{loader: 'file-loader'}]
+		}
+	],
+};
+
+const resolve = {
+	alias: {
+		// Define directory with alias name. usage:
+		// import AxiosMethod from 'utilis/AxiosMethod';
+		'src': path.resolve(__dirname, '../src/'),
+		'utilis': path.resolve(__dirname, '../src/utilis'),
+		'helper': path.resolve(__dirname, '../src/helper'),
+		'assets': path.resolve(__dirname, '../src/assets'),
+	}
+};
+
+const optimization = {
+	splitChunks: {
+		chunks: 'all'
+	}
+};
+
+const devServer = {
+	port: 8000,
+	historyApiFallback: true,
+	contentBase: path.join(__dirname, '../public'),
+	host: '0.0.0.0',
+	publicPath: '/',
+};
+
+const performance = {
+	hints: 'warning',
+	maxEntrypointSize: 4000000,
+	maxAssetSize: 4000000,
+};
+module.exports = {
+	mode: 'production',
+	entry: entry,
+	output: output,
+	module: moduleRules,
+	resolve: resolve,
+	optimization: optimization,
+	performance: performance,
+	plugins: [
+		HtmlWebpackPluginConfig,
+	],
+
+}
--- config/webpack.dev.config.js
+++ config/webpack.dev.config.js
@@ -1,54 +1,28 @@
-const webpack = require('webpack');
 
-// 读取同一目录下的 base config
-const config = require('./webpack.base.config');
+const merge = require('webpack-merge');
+const webpackBaseConfig = require('./webpack.base.config.js');
+const path = require('path');
 
-// 添加 webpack-dev-server 相关的配置项
-config.devServer = {
-  contentBase: './',
-  hot: true,
-  publicPath: '/assets/'
-};
-// 有关 Webpack 的 API 本地代理，另请参考 https://webpack.github.io/docs/webpack-dev-server.html#proxy
+const devServer = {
+	port: 8000,
+	historyApiFallback: true,
+	contentBase: path.join(__dirname, '../public'),
+	host: '0.0.0.0',
+	publicPath: '/',
 
-config.module.rules.push(
-  {
-    test: /\.less$/,
-    use: [
-      'style-loader',
-      'css-loader',
-      'less-loader'
-    ],
-    exclude: /node_modules/
-  }
-);
-
-// 真实场景中，React、jQuery 等优先走全站的 CDN，所以要放在 externals 中
-config.externals = {
-  react: 'React',
-  'react-dom': 'ReactDOM'
 };
 
-// 添加 Sourcemap 支持
-config.plugins.push(
-  new webpack.SourceMapDevToolPlugin({
-    filename: '[file].map',
-    exclude: ['vendor.js'] // vendor 通常不需要 sourcemap
-  })
-);
+module.exports = merge(webpackBaseConfig, {
+	mode: 'development',
+	devServer: devServer,
 
-// Hot module replacement
-Object.keys(config.entry).forEach((key) => {
-  // 这里有一个私有的约定，如果 entry 是一个数组，则证明它需要被 hot module replace
-  if (Array.isArray(config.entry[key])) {
-    config.entry[key].unshift(
-      'webpack-dev-server/client?http://0.0.0.0:8080',
-      'webpack/hot/only-dev-server'
-    );
-  }
+	// watch: true,
+	// watchOptions: {
+	// 	aggregateTimeout: 300,
+	// 	poll: 1000,
+	// 	ignored: ['node_modules'],
+	// },
+
+
+
 });
-config.plugins.push(
-  new webpack.HotModuleReplacementPlugin()
-);
-
-module.exports = config;
