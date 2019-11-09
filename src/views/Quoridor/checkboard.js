060c9b5246b54b83c9b6e0f159ff7b7348782249
DESKTOP-BDL7B3E\Jinhui <rfd3344@gmail.com>
2019-11-09 23:32:13 +1100

    1

--- src/views/Quoridor/checkboard.js
+++ src/views/Quoridor/checkboard.js
@@ -5,9 +5,9 @@ import { AddBoard } from './Redux.js'
 
 
 const CheckBoard = ({Hori_Gaps,Ver_Gaps, AddBoard }) => {
-	let index = [0,1,2,3,4,5,6,7,8]
-	return(
-		<div id="CheckBoard"	class="alert alert-secondary">
+	const index = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+	return (
+		<div id="CheckBoard" className="alert alert-secondary">
 			{
 				index.map( (row) =>(
 					<div className="row">
@@ -17,7 +17,7 @@ const CheckBoard = ({Hori_Gaps,Ver_Gaps, AddBoard }) => {
 									<div className={ Hori_Gaps[row][col]?'bottom active':'bottom'}
 										onClick={()=>AddBoard(row,col,'Hor')} ></div>
 									<div className={ Ver_Gaps[row][col]?'right active':'right'}
-										onClick={()=>AddBoard(row,col,'Ver')}	 ></div>
+										onClick={()=>AddBoard(row,col,'Ver')} ></div>
 								</div>
 							))
 						}
@@ -26,14 +26,16 @@ const CheckBoard = ({Hori_Gaps,Ver_Gaps, AddBoard }) => {
 			}
 		</div>
 	);
-}
+};
 
 const mapStateToProps = (state) => ({
 	Hori_Gaps: state.Quoridor.Hori_Gaps,
 	Ver_Gaps: state.Quoridor.Ver_Gaps,
 	completed: state.Quoridor.completed
-})
-const mapDispatchToProps = dispatch => ({
-	AddBoard: (x,y,dir) => dispatch( AddBoard(x,y,dir) )
-})
-export default connect( mapStateToProps, mapDispatchToProps )(CheckBoard)
+});
+
+const mapDispatchToProps = (dispatch) => ({
+	AddBoard: (x, y, dir) => dispatch(AddBoard(x, y, dir))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(CheckBoard);
